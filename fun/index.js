2cecb4ecec0fe4d6a41a274e0dad1d0505b86a92
arka1002 <arka.mcu700@gmail.com>
2023-11-25 19:24:47 +0530

    come here, if things turn dull

--- fun/index.js
+++ fun/index.js
@@ -1,1 +1,11 @@
-foo['__proto__']
+const foo = {
+    bar: 123,
+    bazz: {
+      good: 12,
+      bad: 13,
+      wow: {
+        worse: 156
+      }
+    }
+  };
+  const { bar, bazz, bazz: { bad }, bazz: { wow: { worse } } } = foo;

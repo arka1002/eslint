0489e531118fd53ce46136ce6559d2b70bca271a
arka1002 <arka.mcu700@gmail.com>
2023-12-06 10:40:21 +0530

    i did something ig, but its still not enough though ???

--- fun/eslint.config.js
+++ fun/eslint.config.js
@@ -2,6 +2,7 @@ export default [
     {
         rules: {
             "no-restricted-properties": ["error", {
+                "object": "foo",
                 "property": "bad",
             }]
         }
